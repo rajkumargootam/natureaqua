532b96b73dd62fa93f12ce0077e8ed03bc8c7c2b
rajkumargootam <rajkumar.gootam@gmail.com>
2020-08-13 23:37:43 +0530

    modified the routes and added new routes(search,search-date,delete,update)

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -5,12 +5,20 @@ import { ProductsComponent } from './products.component';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { ViewAllProductsByCategoryComponent } from './view-all-products-by-category/view-all-products-by-category.component';
+import { ViewAllProductsByDateComponent } from './view-all-products-by-date/view-all-products-by-date.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
   { path: 'create-product', component: CreateProductComponent },
-  { path: 'view-product', component: ViewProductComponent },
   { path: 'list-products', component: ViewAllProductsComponent },
+  { path: 'search', component: ViewAllProductsByCategoryComponent },
+  { path: 'search-date', component: ViewAllProductsByDateComponent },
+  { path: 'delete-product', component: DeleteProductComponent },
+  { path: 'view-product/:id', component: ViewProductComponent },
+  { path: 'update-product/:id', component: UpdateProductComponent },
 
 ];
 
