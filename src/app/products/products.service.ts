67d035ccb73074d7b5ca84161c994272dabc9b31
rajkumargootam <rajkumar.gootam@gmail.com>
2020-08-14 01:05:51 +0530

    Created services for (createProduct,viewProduct,updateProduct,deleteProduct,searchCategoryProduct,searchDateProduct).

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,10 +1,47 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  createProduct(productBody){
+    const productUrl = 'http://localhost:3000/products';
+    return this.httpClient.post(productUrl,productBody); //return Observanle
+
+  }
+
+  viewProduct(productId){
+    const productUrl = 'http://localhost:3000/product/'+productId;
+    return this.httpClient.get(productUrl); //return Observanle
+
+  }
+
+  updateProduct(productId, productBody){
+    const productUrl = 'http://localhost:3000/product/'+productId;
+    return this.httpClient.put(productUrl,productBody); //return Observanle
+
+  }
+
+  deleteProduct(productId){
+    const productUrl = 'http://localhost:3000/product/'+productId;
+    return this.httpClient.delete(productUrl); //return Observanle
+
+  }
+
+  searchCategoryProduct(categoryId){
+    const productUrl = 'http://localhost:3000/products/categoryId='+categoryId;
+    return this.httpClient.get(productUrl); //return Observanle
+
+  }
+
+  searchDateProduct(dateParam){
+    const productUrl = 'http://localhost:3000/products/date='+categoryId;
+    return this.httpClient.get(productUrl); //return Observanle
+
+  }
+
 }
