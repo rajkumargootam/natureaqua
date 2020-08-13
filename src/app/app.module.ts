35726827bc3409ea84a2fa04be329760a9a264df
rajkumargootam <rajkumar.gootam@gmail.com>
2020-08-14 00:50:59 +0530

    Generated new service with the name products and imported the HttpClientModule in app.module.ts .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdersModule } from './orders/orders.module';
 import { SiteFrameworkModule } from './site-framework/site-framework.module';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +13,7 @@ import { SiteFrameworkModule } from './site-framework/site-framework.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     OrdersModule,
     SiteFrameworkModule
   ],
